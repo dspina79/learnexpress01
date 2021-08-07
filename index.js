897f48d304ff703a9e534e782bad7bd81295c694
Dave Spina <dspina79@gmail.com>
2021-08-07 12:16:32 -0400

    Basic setup and API

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const arrayData = [1, 2, 3];
+
+app.get('/', (req, res) => {
+    res.send("Hello <strong>World!</strong>");
+});
+
+app.get('/api/arrayData', (req, res) => {
+    arrayData.push(arrayData[arrayData.length - 1] + arrayData[arrayData.length - 2]);
+    res.send(arrayData);
+});
+
+app.listen(port, () => {
+    console.log("Listening on port 3000!");
+});
